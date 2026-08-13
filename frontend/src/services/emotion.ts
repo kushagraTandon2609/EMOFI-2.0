d3ad0349ee82f846675f581475f708edcefa8b18
kushagraTandon2609 <kushagratandonvsec@gmail.com>
2026-08-13 22:48:01 +0530

    emotion ts

--- frontend/src/services/emotion.ts
+++ frontend/src/services/emotion.ts
@@ -1,9 +1,15 @@
 import api from "./api";
 
 export const detectEmotion = async (image: string) => {
-    const response = await api.post("/detect", {
-        image,
-    });
+    const response = await api.post(
+        "/detect",
+        {
+            image,
+        },
+        {
+            timeout: 15000,
+        }
+    );
 
     return response.data;
 };
