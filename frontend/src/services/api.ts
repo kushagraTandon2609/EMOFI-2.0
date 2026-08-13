b0b86a1f81012207f8089a6621e47b4a50a75c91
kushagraTandon2609 <kushagratandonvsec@gmail.com>
2026-08-13 23:05:01 +0530

    api ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,27 +1,29 @@
 import axios from "axios";
 
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "http://127.0.0.1:5000/api";
+
 const api = axios.create({
-    baseURL:
-        import.meta.env.VITE_API_URL ||
-        "http://127.0.0.1:5000/api",
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("token");
+  (config) => {
+    const token = localStorage.getItem("token");
 
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export default api;
