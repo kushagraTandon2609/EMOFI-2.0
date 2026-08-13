a6e376a96d0cbd18577aee92a7a0b2315029b1b5
kushagraTandon2609 <kushagratandonvsec@gmail.com>
2026-08-13 16:40:50 +0530

    api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,17 +1,25 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://emofi-2-0-backend.onrender.com/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-api.interceptors.request.use((config) => {
+api.interceptors.request.use(
+  (config) => {
     const token = localStorage.getItem("token");
 
     if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
 
     return config;
-});
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
